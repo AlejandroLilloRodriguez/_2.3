b68d5a2c8b36fb145fc8ec1390953e6192b91ce4
AlejandroLilloRodriguez <alejandro.lillorodriguez@usp.ceu.es>
2023-02-16 13:18:49 +0100

    Ejercicio 3 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,16 @@
-package aplicacion;public class Principal {
+package aplicacion;
+
+import dominio.Ejercicio3;
+import java.util.Scanner;
+public class Principal {
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+        int base;
+        int exponente;
+        System.out.println("Introduzca un exponente");
+        exponente=sc.nextInt();
+        System.out.print("Introduzca el numero del que quiera calcular la base");
+        base=sc.nextInt();
+        System.out.print(Ejercicio3.potencia(base,exponente));
+    }
 }
--- src/dominio/Ejercicio3.java
+++ src/dominio/Ejercicio3.java
@@ -1,2 +1,10 @@
-package dominio;public class Ejercicio3 {
+package dominio;
+
+public class Ejercicio3 {
+    public static int potencia(int base,int exponente){
+        if(exponente==0)
+            return 1;
+        else
+            return base*potencia(base,exponente-1);
+    }
 }
